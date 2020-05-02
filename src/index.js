5e6f62d90e2334dd4562a4b8a7155f7353cc6c8e
gutierrpdev <gutierrp.dev@gmail.com>
2020-05-02 20:42:33 +0200

    quick fix

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', yourExactHostname);
+  res.header('Access-Control-Allow-Origin', 'https://intelligence-assessment-games.herokuapp.com');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
